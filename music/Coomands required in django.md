edb75ccb57e66da12fab720edfc22bb8c44bce3f
deepak rana <34414032+deepak-891@users.noreply.github.com>
2019-04-18 19:16:13 +0530

    Add files via upload

---
+++ music/Coomands required in django.md
@@ -0,0 +1,7 @@
+Coomands required in django 
+
+python manage.py runserver 
+python manage.py migrate
+python manage.py makemigration music
+
+python manage.py sqlmigrate music 0001
--- music/models.py
+++ music/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+	"""docstring for Album"""
+	artist = models.CharField(max_length=250)
+	album_title=models.CharField(max_length=250)
+	genre = models.CharField(max_length=100)
+	album_logo = models.CharField(max_length=1000)
+
+	def __str__():
+		return self.album_title + '-' + self.artist
+
+
+class Song(models.Model):
+	album = models.ForeignKey(Album,on_delete=models.CASCADE)
+	file_type = models.CharField(max_length=10)
+	song_title = models.CharField(max_length=250)	
