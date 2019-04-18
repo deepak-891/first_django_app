fbcb26469aa1ccee864d45508db9c0e731f92d89
deepak rana <34414032+deepak-891@users.noreply.github.com>
2019-04-18 20:41:11 +0530

    Add files via upload

--- music/models.py
+++ music/models.py
@@ -9,7 +9,7 @@ class Album(models.Model):
 	genre = models.CharField(max_length=100)
 	album_logo = models.CharField(max_length=1000)
 
-	def __str__():
+	def __str__(self):
 		return self.album_title + '-' + self.artist
 
 
@@ -17,3 +17,6 @@ class Song(models.Model):
 	album = models.ForeignKey(Album,on_delete=models.CASCADE)
 	file_type = models.CharField(max_length=10)
 	song_title = models.CharField(max_length=250)	
+
+	def  __str__(self):
+		return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,6 +1,18 @@
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Album 
+from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
-	return HttpResponse("<h1>This is the music home pge</h1>")
+	
+	all_albums = Album.objects.all()
+	return render(request,'music/index.html',{'all_albums': all_albums})
+
+def detail(request, album_id):
+	try:
+		album = Album.objects.get(pk=album_id)
+	except Album.DoesNotExist:
+		raise Http404("Album Does Not Exist")
+	return render(request,'music/detail.html',{'album': album})
+	
